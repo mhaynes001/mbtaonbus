be9faf857a15fce8cbb7676302295e0bba38650c
Mike Haynes <mhaynes001@gmail.com>
2019-02-28 06:53:43 -0600

    Initial GTFS-RT conversion
    Initial attempt at doing gtfs-rt.  Need to test on Ubuntu server.

--- main_functions.py
+++ main_functions.py
@@ -1,16 +1,23 @@
 # mbtaonbus
 # main_functions.py
 # Created by M. Haynes
-# Late July 2018 & January 2019 (Flask)
+# Late July 2018 & January 2019 now Feb 2019 (GTFS-rt) (Flask)
 #
 
-from urllib.request import Request, urlopen
-import gzip, json, datetime, urllib, math, os
-import pandas as pd
+from google.transit import gtfs_realtime_pb2 as gtfs_rt
+from protobuf_to_dict import protobuf_to_dict as pb_to_dict
+import math, os, requests as req, datetime as dt, pandas as pd
 
-# Get the MBTA API key from the system variables:
-apikey = os.environ.get('MBTA_KEY')
-mainurl='https://api-v3.mbta.com/'
+def timestamp_convert(posix_time):
+	return dt.datetime.fromtimestamp(posix_time).strftime('%Y-%m-%d %H:%M:%S')
+
+veh_feed = gtfs_rt.FeedMessage()
+trip_feed = gtfs_rt.FeedMessage()
+alert_feed = gtfs_rt.FeedMessage()
+
+# Get the URL from the system variables:
+#mainurl=   #os.environ.get('GTFS_URL')
+gtfs_rt_url = 'https://cdn.mbta.com/realtime/'
 data_path = '/home/user/mbtaonbus/data/'
 
 def getmapboxkey():
@@ -36,34 +43,10 @@ trip_patterns = pd.read_csv(data_path+'trip_patterns.txt', dtype={'trip_id': int
 unique_stopstrings = pd.read_csv(data_path+'unique_stopstrings.txt', dtype={'pattern': int, 'stops': str})
 unique_stopstrings.stops.apply(lambda x: x[1:-1].split(',') )
 
-#Function that gets data compressed and then uncompresses it to JSON, returns JSON:
-def apiget(midurl,endurl=''):
-    fullurl = mainurl + midurl + '?api_key=' + apikey + endurl
-    #print(fullurl)
-    req = Request(fullurl)
-    req.add_header('Accept-Encoding', 'gzip')
-    try:
-        response = urlopen(req)
-    except:
-        return  # Ideally return an error code here and then print a different page
-    try:
-        content = gzip.decompress(response.read())    # If it fails run the uncompressed version
-    except:
-        return
-    json_out = json.loads(content.decode('utf-8'))
-    return json_out
-
-# Sometimes it won't compress so this is for those times, returns JSON
-def apiget_nocompress(midurl,endurl=''):
-    fullurl=mainurl+midurl+'?api_key='+apikey+endurl
-    request = urllib.request.urlopen(fullurl)
-    json_out = json.loads(request.read().decode())
-    return json_out
-
 # Function to convert time to a nice string for presentation:
 def converttime(time_in):
-    if time_in is None: return
-    if time_in[-3:-2] == ":":
+	if time_in is None: return
+	if time_in[-3:-2] == ":":
         time_in = time_in[:-3]+time_in[-2:]  #remove the last colon from UTC offset
     time_out = datetime.datetime.strptime(time_in,"%Y-%m-%dT%H:%M:%S%z")
     time_out = time_out.strftime('%-I:%M:%S')
@@ -86,131 +69,124 @@ xstr = lambda s: '' if s is None else str(s)
 
 # Function to return a list of active vehicles (for the main page to select a bus)
 def getvehicles():
-    i = 0
-    veh_list = []
-    veh_data = apiget('vehicles')
-    for i in range(len(veh_data['data'])):   # From 0 to length of vehicles list
-        if veh_data['data'][i]['id'][0] == 'y' and veh_data['data'][i]['id'][1] != 's':
-            veh_list.append(veh_data['data'][i]['id'][1:])
-    return veh_list
+    veh_response = req.get(gtfs_rt_url+'VehiclePositions.pb')
+	veh_feed.ParseFromString(veh_response.content)
+	# convert to dict from protobuf feed
+	veh_dict = pb_to_dict(veh_feed)
+
+	veh_list = []
+     	for x in veh_dict['entity']:
+        	veh_list.append(x['vehicle']['vehicle']['label'])
+     	return veh_list
 
 # Function to get the basic bus data (test for data to ensure it is out there)
 def getbasicdata(veh):
-    veh_data = apiget('vehicles/y'+str(veh))
-    if veh_data is not None:
-        return processveh_data(veh_data)
-    else:
-        return
+    veh_response = req.get(gtfs_rt_url+'VehiclePositions.pb')
+	veh_feed.ParseFromString(veh_response.content)
+	# convert to dict from protobuf feed
+	veh_dict = pb_to_dict(veh_feed)
+	veh_match = next((x for x in veh_dict['entity'] if x['vehicle']['vehicle']['label'] == veh), 'None')
+	veh_match = veh_match['vehicle']
+	
+	if veh_data is not None:
+        	return processveh_data(veh_match)
+    	else:
+        	return
 
 # Function to process the data into dictionary for webpage.
 def processveh_data(veh_data):
     # Current time:
-    currentDT = datetime.datetime.now() # + datetime.timedelta(hours=1)  # +1 to get to EST
+	currentDT = dt.datetime.now() # + datetime.timedelta(hours=1)  # +1 to get to EST
 
-    # Get the Trip Data from the API:
-    trip_id =  veh_data['data']['relationships']['trip']['data']['id']
-    trip_data = apiget('trips/'+trip_id)
+	route_id = veh_data['trip']['route_id']
+	trip_id = veh_data['trip']['trip_id']
+	lat = round(veh_data['position']['latitude'],6)
+	long = round(veh_data['position']['longitude'],6)
+	heading = veh_data['position']['bearing']
+	location = str(lat) +','+ str(long) +','+ str(heading)
+	current_status = veh_data['current_status']   ### Need to look these up. 
 
-    # Current Bus details:
-    try:
-        route_id = veh_data['data']['relationships']['route']['data']['id']
-    except:
-        route_id = '0'
-    try:
-        dir_id = veh_data['data']['attributes']['direction_id']
-    except:
-        dir_id = 0
-    try:
-        stop_id = veh_data['data']['relationships']['stop']['data']['id']
-    except:
-        stop_id = 0
+	veh_data_out = {
+    	"vehicle_number" : veh_data['vehicle']['label'],
+    	"lat" : lat, "long" : long, "heading" : heading, "location" : location,
+    	"updated_at" : timestamp_convert(veh_data['timestamp']),
+    	"current_time" : currentDT.strftime('%-I:%M:%S %p'),
+    	"current_status" : current_status,  #.replace("_", " ").lower()
 
-    try:
-    	shape_id = trip_data['data']['relationships']['shape']['data']['id']
-    except:
-        shape_id = 0
-
-    lat = veh_data['data']['attributes']['latitude']
-    long = veh_data['data']['attributes']['longitude']
-    heading = veh_data['data']['attributes']['bearing']
-    location = str(lat) +','+ str(long) +','+ str(heading)
-    current_status = veh_data['data']['attributes']['current_status']
-
-    veh_data_out = {
-        "vehicle_number" : int(veh_data['data']['attributes']['label']),
-        "lat" : lat, "long" : long, "heading" : heading, "location" : location,
-	"updated_at" : converttime(veh_data['data']['attributes']['updated_at']),
-	"current_time" : currentDT.strftime('%-I:%M:%S %p'),
-	"current_status" : current_status.replace("_", " ").lower(),
-
-	"route_id" : route_id, "dir_id" : dir_id,
-	"stop_id" : stop_id,
-	"stop" : str.upper(xstr(getdata(stop_data,'stop_id',stop_id).get("stop_name"))),
-	"trip_id" : trip_id,
-
-	"route_name" : getdata(route_data,'route_id',route_id).get('route_short_name'),
-	"route_des" : getdata(route_data,'route_id',route_id).get('route_long_name'),
-
-	"headsign" : trip_data['data']['attributes']['headsign'],
-	"shape_id" : shape_id
+    	"route_id" : route_id, 
+    	"dir_id" : veh_data['trip']['direction_id'],
+    	"stop_id" : veh_data['stop_id'],
+    	"stop" : str.upper(xstr(getdata(stop_data,'stop_id',veh_data['stop_id']).get("stop_name"))),
+    	"trip_id" : trip_id,
+    
+    	"route_name" : getdata(route_data,'route_id',route_id).get('route_short_name'),
+    	"route_des" : getdata(route_data,'route_id',route_id).get('route_long_name'),
+    	"headsign" : '',  ## Does not exist in GTFS-rt feed
+    	"shape_id" : getdata(trip_patterns,'trip_id',trip_id).get('shape_id')
     }
     return veh_data_out
 
 # Function to get the predictions and return a dictionary (ideally of size 8)
 def getpredictions(trip_id):
-	pred_results = apiget('predictions','&filter[trip]='+trip_id)
-	pred_data = []
-	n = 0  # The prediction position in the main list
-	m = 0  # The prediction position in what is returned
+    trip_response = req.get(gtfs_rt_url+'TripUpdates.pb')
+    trip_feed.ParseFromString(trip_response.content)
+    trip_dict = pb_to_dict(trip_feed)
+    
+    trip_match = next((x for x in trip_dict['entity'] if x['id'] == trip_id), None)
+    #print(trip_match)
+    try:
+        pred_results = trip_match['trip_update']['stop_time_update']
+    except: 
+        return
+    
+    pred_data = []
+    n = 0  # The prediction position in the main list
+    m = 0  # The prediction position in what is returned
 
-	## Get the length of the pred_results array and evenly obtain 8 predictions
-	## If an odd number should not show stop #8 so that we always have 8
-	try:
-		pred_length = len(pred_results['data'])
-	except:
-		pred_length = 0
-		return
-	factor = math.trunc(pred_length/8)+1
+    ## Get the length of the pred_results array and evenly obtain 8 predictions
+    ## If an odd number should not show stop #8 so that we always have 8
+    pred_length = len(pred_data)
+    factor = math.trunc(pred_length/8)+1
 
-	if pred_length < 10: factor = 1  # If 9 or less factor is 1
-	# Can just make the array so it essentially is and n in []
-	# Special case array for six n values. store them in another array.
+    if pred_length < 10: factor = 1  # If 9 or less factor is 1
+    # Can just make the array so it essentially is and n in []
+    # Special case array for six n values. store them in another array.
 
-	for result in pred_results['data']:
-		n += 1
-		arr_time = converttime(result['attributes']['arrival_time'])
-		dep_time = converttime(result['attributes']['departure_time'])
-		time = dep_time if arr_time is None else arr_time
-		stop_id = result['relationships']['stop']['data']['id']
+    for result in pred_results:
+        n += 1
+        if 'arival' in result.keys(): 
+            time = timestamp_convert(result['arival']['time'])
+        if 'departure' in result.keys():
+            time = timestamp_convert(result['departure']['time'])
+        stop_id = result['stop_id']
 
-		if time is not None and (n%factor == 0 or n == pred_length or n == 1):
-			m += 1
-		    # Drop the seconds but add a "+" if it is over 30 seconds (2nd half of minute)
-			time = time[:-3] if int(time[-2:]) < 30 else time[:-3]+'+'
-			pred_data.append({
-				"m" : m,  # Dictionary index
-    			"n" : n,  # Overall prediction index
-    			"n_txt" : ordinal(n), # Prediction as a ordinal number
-    			"stop_seq" : result['attributes']['stop_sequence'],
-    			"stop_id" : stop_id,
-    			"stop" : str.upper(getdata(stop_data,'stop_id',stop_id).get("stop_name")),
-    			"time" : time
-    		})
+        if time is not None and (n%factor == 0 or n == pred_length or n == 1):
+            m += 1
+            # Drop the seconds but add a "+" if it is over 30 seconds (2nd half of minute)
+            time = time[:-3] if time[-2:] < '30' else time[:-3]+'+'
+            pred_data.append({
+                "m" : m,  # List index
+                "n" : n,  # Overall prediction index
+                "n_txt" : ordinal(n), # Prediction as a ordinal number
+                "stop_seq" : result['stop_sequence'],
+                "stop_id" : stop_id,
+                "stop" : str.upper(getdata(stop_data,'stop_id',stop_id).get("stop_name")),
+                "time" : time
+            })
 
-	if pred_data[0]["n_txt"] == '1st': pred_data[0]["n_txt"] = 'Next'
+    if pred_data[0]["n_txt"] == '1st': pred_data[0]["n_txt"] = 'Next'
 
-	#If pred_data length is 9 drop the 8th item:
-	if len(pred_data) > 8:
-		del pred_data[7]
-	# If pred_data length is 7 slide 2nd back into the dictionary:
+    #If pred_data length is 9 drop the 8th item:
+    if len(pred_data) > 8:
+        del pred_data[7]
+    # If pred_data length is 7 slide 2nd back into the dictionary:
 
-	return pred_data
+    return pred_data
 
 # Function to return the shape of a route from the GTFS shape.txt file in a pandas df:
 def getshape(shape_id):
 #	print("shapeID:",shape_id)
 	shape = shape_data[shape_data.shape_id == shape_id].values
-        # SHOULD round lat/longs
 	return shape
 
 # Function to return the stops of a given trip:
@@ -233,7 +209,8 @@ def getstops(trip_id_in):
 
 # Function to return the alerts for a given route:
 def getalerts(route_id):
-	alert_data = apiget('alerts','&filter[route]='+route_id)
+	#alert_data = apiget('alerts','&filter[route]='+route_id)
+	alert_data = None   # For now turn this off...
 	if alert_data is not None:
 		alert = alert_data['data'][0]['attributes']['short_header']
 	else:
